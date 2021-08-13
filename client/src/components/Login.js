ab46e5fc86204f1545d73f758c9efcec7bdf4921
Ana Nytochka <70048765+starship-z@users.noreply.github.com>
2021-08-12 21:44:04 -0700

    Add logout

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,29 +1,41 @@
 import React, {useState} from 'react'
 
 function Login() {
-    let [text, setText] = useState("");
+    let [username, setUsername] = useState("");
     let [password, setPass] = useState("");
-    let [loggedIn, setLoggedIn] = useState(false);
+    let [user, setUser] = useState({});
 
-    function loginButton() {
-        setLoggedIn(true);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setUsername("");
+        setPass("");
+        const new_user = {username: username, password: password, status: true};
+        setUser(new_user);
+    }
+
+    const handleLogOut = () => {
+        const new_user = {};
+        setUser(new_user);
     }
 
     return (
         <div>
-            <h1>Login</h1>
-            <h5>Username</h5>
-            <input type="text" value={text} onChange={e => setText(e.target.value)}></input>
-            <h5>Password</h5>
-            <input type="password" value={password} onChange={e=> setPass(e.target.value)}></input>
-            <br></br>
-            <button onClick={loginButton}>Submit</button>
-            <br></br>
-            <h5>{text}</h5>
-            <h5>{password}</h5>
-            <h5>Status: {loggedIn ? "True" : "False"}</h5>
+            <form onSubmit={e => handleSubmit(e)}>    
+                <h1>Login</h1>
+                <h5>Username</h5>
+                <input type="text" value={username} onChange={e => setUsername(e.target.value)}></input>
+                <h5>Password</h5>
+                <input type="password" value={password} onChange={e=> setPass(e.target.value)}></input>
+                <br></br>
+                <button type="submit">Submit</button>
+                <br></br>
+                <h5>Username: {user.username}</h5>
+                <h5>Password: {user.password}</h5>
+                <h5>Status: {user.status ? "True" : "False"}</h5>
+            </form>
+            <button onClick={e => handleLogOut()}>Log Out</button>
         </div>
     )
-}
+} 
 
 export default Login
