d4bacd775376abbeea7a8c36988a6a765851a6f9
Ana Nytochka <70048765+starship-z@users.noreply.github.com>
2021-08-14 12:49:28 -0700

    Add counter component

--- client/src/components/Counter.js
+++ client/src/components/Counter.js
@@ -2,14 +2,26 @@ import React, {useState} from 'react'
 
 function Counter() {
     let [count, setCount] = useState(0);
+    let [text, setText] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let temp = parseInt(text, 10);
+        setCount(temp);
+        setText("");
+    }
 
     return (
         <div>
             <h1>Counter: {count}</h1>
             <button onClick={e => setCount(count+1)}>increase</button>
             <button onClick={e => setCount(count-1)}>decrease</button>
-        </div>
-    )
-}
-
-export default Counter
+            <form onSubmit={handleSubmit}>
+                <input type="text" onChange={e => setText(e.target.value)} value={text}></input>
+                <button type="submit">Enter</button>
+            </form>
+            </div>
+            )
+        }
+        
+        export default Counter
